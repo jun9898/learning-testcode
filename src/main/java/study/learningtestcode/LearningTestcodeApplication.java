c30f913e61208f23fc3b5d14aca58e697f7a593b
jun9898 <nice1998@gmail.com>
2024-12-10 22:05:55 +0900

    project init

---
+++ src/main/java/study/learningtestcode/LearningTestcodeApplication.java
@@ -0,0 +1,13 @@
+package study.learningtestcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningTestcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningTestcodeApplication.class, args);
+	}
+
+}
